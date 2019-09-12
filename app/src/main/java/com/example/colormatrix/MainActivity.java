9ff3f5245affcc75ee2984ea3636216f11261d1a
shadslattery <shadslattery@gmail.com>
2019-09-11 22:32:31 -0400

    Buttons upto Number Ten, and Continuing

--- app/src/main/java/com/example/colormatrix/MainActivity.java
+++ app/src/main/java/com/example/colormatrix/MainActivity.java
@@ -7,7 +7,7 @@ import androidx.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
     @Override
-protected void onCreate((@Nullable Bundle saveInstanceState)
+        protected void onCreate(@Nullable Bundle saveInstanceState)
     {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_color_matrix);
