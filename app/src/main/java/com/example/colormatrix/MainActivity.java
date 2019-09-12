f6d0f9ca3f5d766bad7dcaed60b9ab125326da39
shadslattery <shadslattery@gmail.com>
2019-09-11 21:27:26 -0400

    Initial commit

---
+++ app/src/main/java/com/example/colormatrix/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.colormatrix;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+protected void onCreate((@Nullable Bundle saveInstanceState)
+    {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_color_matrix);
+    }
+}
